0d5e78263d2785e231d73ec485cfecce6b3f77f6
javkhlantugs <jay.mudgil@gmail.com>
2016-12-21 05:02:28 -0500

    finished designer/dev des/dev finished education

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,12 +1,24 @@
 $(document).on('turbolinks:load', function() {
 
 	$(".js-designer").click(function(){
-		$(".designer-window").toggleClass('window-inactive window-active')
-		$(this).toggleClass('title-up title-center')
+		$(".designer-window").toggleClass('window-inactive window-active');
+		$(this).toggleClass('title-up title-center');
+		$(".designer-items").toggleClass('.designer-items-inactive', fadeDesigner());
 	});
 
 	$(".js-developer").click(function () {
-		$(".developer-window").toggleClass('window-active window-inactive')
-		$(this).toggleClass('title-up title-center')
+		$(".developer-window").toggleClass('window-active window-inactive');
+		$(this).toggleClass('title-up title-center');
+		$('.developer-items').toggleClass('developer-items-inactive,', fadeDeveloper());
 	})
+
+	
 });
+
+function fadeDesigner() {
+	$(".designer-items").fadeToggle('fast', 'linear');
+}
+
+function fadeDeveloper() {
+	$(".developer-items").fadeToggle('fast', 'linear')
+}
